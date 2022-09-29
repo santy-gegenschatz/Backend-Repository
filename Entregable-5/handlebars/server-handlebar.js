c8cf257a19ecf7fa0c15b53223ee81a9aca2bf89
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-09-29 16:05:00 -0300

    Handlebars version finished

--- Entregable-5/handlebars/server-handlebar.js
+++ Entregable-5/handlebars/server-handlebar.js
@@ -1,8 +1,8 @@
 // Data imports
-const p1 = {id : 1, title : 'prodcut #1', price : 10, thumbnail : 'url'}
-const p2 = {id : 2, title : 'prodcut #2', price : 20, thumbnail : 'url'}
-const p3 = {id : 3, title : 'prodcut #3', price : 150, thumbnail : 'url'}
-const p4 = {id : 4, title : 'prodcut #4', price : 250, thumbnail : 'url'}
+const p1 = {id : 1, title : 'Calculator', price : 10, thumbnail : 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'}
+const p2 = {id : 2, title : 'Clock', price : 20, thumbnail : 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/clock-128.png'}
+const p3 = {id : 3, title : 'Shirt', price : 40, thumbnail : 'https://cdn3.iconfinder.com/data/icons/remixicon-others/24/shirt-fill-128.png'}
+const p4 = {id : 4, title : 'Shoes', price : 30, thumbnail : 'https://cdn4.iconfinder.com/data/icons/fashion-industry-set/88/Di-128.png'}
 const products = [p1, p2, p3, p4]
 
 console.log(products);
