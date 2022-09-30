c8841a36d25a66625c01d0fb8afa14b67835d382
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-09-29 22:53:40 -0300

    pug now working

--- Entregable-5/handlebars/server.js
+++ Entregable-5/handlebars/server.js
@@ -1,19 +1,12 @@
-// Data imports
-const p1 = {id : 1, title : 'Calculator', price : 10, thumbnail : 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'}
-const p2 = {id : 2, title : 'Clock', price : 20, thumbnail : 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/clock-128.png'}
-const p3 = {id : 3, title : 'Shirt', price : 40, thumbnail : 'https://cdn3.iconfinder.com/data/icons/remixicon-others/24/shirt-fill-128.png'}
-const p4 = {id : 4, title : 'Shoes', price : 30, thumbnail : 'https://cdn4.iconfinder.com/data/icons/fashion-industry-set/88/Di-128.png'}
-const products = [p1, p2, p3, p4]
-
-console.log(products);
-
+// Get the data
+const products = require('./data/index.js')
 // Plain vanilla server with express
 const express = require('express');
 const app = express();
 
 // Extract router from express
 const {Router} = express
-const router = Router()
+const router = Router()//
 
 // Use json to receive post requests
 app.use(express.json())
---
+++ Entregable-5/pug/server.js
@@ -0,0 +1,154 @@
+// Get the data
+const products = require('./data/index.js')
+console.log(products);
+// Plain vanilla server with express
+const express = require('express');
+const app = express();
+
+// Extract router from express
+const {Router} = express
+const router = Router()//
+
+// Use json to receive post requests
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(express.static('public'))
+
+// Set up the app.use
+app.use('/api/products', router)
+
+// Pug Config
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+// Common url's
+app.get('/', (req, res) => {
+    res.render('form.pug', {products: products, render : true})
+})
+
+app.get('/products', (req, res) => {
+    res.render('products.pug', {products: products, render : products.length!==0})
+})
+
+app.post(('/products'), (req, res) => {
+    const {title, price, thumbnail} = req.body
+    console.log(req.body);
+    if (title && price && thumbnail) {
+        const newProduct = {
+            title : title,
+            price : price,
+            thumbnail : thumbnail,
+            id : products.length + 1
+        }
+        // Add the product to the array
+        products.push(newProduct)
+        res.redirect('/products')
+    }
+})
+
+// Router url's
+router.get('/', (req, res) => {
+    res.status(200).json({products: products})
+})
+
+// Get products by id
+router.get('/:id', (req, res) => {
+    // We get the params with a destructuring, as follows:
+    const { id } = req.params
+    // We find the product in the array of products
+    const selectedProduct = products.find(prod => prod.id === Number(id))
+    if (selectedProduct !== undefined) {
+        res.status(200).json({
+            product : selectedProduct
+        })
+    } else {
+        res.status(400).json({
+            error : "No product matches that id"
+        })
+    }
+})
+
+// Add a product to the array
+router.post('/', (req, res) => {
+    const {title, price, thumbnail} = req.body
+    console.log(req);
+    console.log(req.body);
+    if (title && price && thumbnail) {
+        const newProduct = {
+            title : title,
+            price : price,
+            thumbnail : thumbnail,
+            id : products.length + 1
+        }
+        // Add the product to the array
+        products.push(newProduct)
+        // Return it through a JSON
+        res.status(200).json({
+            message : 'Successfuly created product',
+            product : newProduct
+        })
+    } else {
+        res.status(400).json({error : 'At least one of the query params is failing'})
+    }
+})
+
+// Update a product that already exists in the db
+router.put('/:id', (req, res) => {
+    // Get the id via req.params
+    const {id} = req.params
+    // Find the product in the array
+    const productToUpdate = products.find( (prod) => prod.id === Number(id))
+    if (productToUpdate !== undefined) {
+        // Find it in the array again, but this time mo
+        let modified = false;
+        let {title, price, thumbnail} = req.body
+        console.log(req.body);
+        //
+        if (title && price && thumbnail) {
+            products.forEach( (prod) => {
+                if (prod.id === Number(id)) {
+                    prod.title = title
+                    prod.price = price
+                    prod.thumbnail = thumbnail
+                    modified = true;
+                }
+            })
+            if (modified) {
+                res.status(200).json({
+                    status : `Successfully updated product with id ${id}`
+                })    
+            }
+        } else {
+            res.status(400).json({
+                error : 'One of the body parameters is faulty'
+            })
+        }
+    } else {
+        res.status(400).json({
+            error : 'The id inserted did not match with any product'
+        })
+    }
+})
+
+// Delete an item
+router.delete('/:id', (req, res) => {
+    const {id} = req.params
+    // Find it in the array and delete it
+    const index = products.findIndex( (prod) => prod.id === Number(id))
+    if (index !== -1) {
+        products.splice(index, 1)
+        res.status(200).json({
+            response : 'Product successfully deleted'
+        })
+    } else {
+        res.status(400).json({
+            error : 'No product matches that id'
+        })
+    }
+})
+
+// Make the server listen on a given port
+const PORT = process.env.PORT || 8080
+app.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`Server running on port ${PORT}`);
+})
