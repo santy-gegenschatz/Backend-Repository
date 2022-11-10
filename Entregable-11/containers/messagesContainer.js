b4b221608b8f51af36a3f60a5a374f7f449a0342
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-11-10 20:33:02 -0300

    [feature] - added normalization features

--- Entregable-11/containers/messagesContainer.js
+++ Entregable-11/containers/messagesContainer.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const util = require('util')
 
 class MessagesContainer {
     constructor(filename) {
@@ -16,7 +17,7 @@ class MessagesContainer {
     }
 
     getMessages() {
-        return this.messages
+        return {payload: this.messages, compression: this.normalizeMessages()}
     }
 
     save(object) {
@@ -41,7 +42,33 @@ class MessagesContainer {
         const response = await this.read()
         console.log(response);
         this.messages = response
+        this.normalizeMessages()
     }
+
+    normalizeMessages() {
+        const normalizr = require('normalizr')
+        const normalize = normalizr.normalize
+        const schema = normalizr.schema
+        const author = new schema.Entity('authors', {}, {idAttribute: 'email'})
+        const message = new schema.Entity('messages', {
+            author: author
+        })
+        const messages = new schema.Entity('array', {
+            messages: [message]
+        })
+        const normalizedData = normalize({id: 1, messages: this.messages}, messages)
+        const print = (obj) => {
+            console.log(util.inspect(obj, false, 12, true));
+        }
+
+        console.log('NormalizedData: ', normalizedData);
+        console.log(normalizedData.entities.messages);
+        console.log('------');
+        const a = JSON.stringify(this.messages).length;
+        const b = JSON.stringify(normalizedData).length;
+        return b/a
+    }
+
 }
 
 module.exports =  new MessagesContainer('messages.txt')
