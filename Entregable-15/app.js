ff055f328171a2e3f00a7610afa7f82d95d5494a
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-12-09 12:18:13 -0300

    [feature] - changes to log features in the forking process

--- Entregable-15/app.js
+++ Entregable-15/app.js
@@ -1,6 +1,5 @@
 const yargs = require('yargs/yargs')(process.argv.slice(2))
 const mode = yargs.argv.mode || 'FORK'
-console.log(mode);
 if (mode === 'FORK') {
     console.log('Initiating with Fork mode, only one thread will run');
     const Server = require('./server/server')
@@ -10,16 +9,16 @@ if (mode === 'FORK') {
     console.log('Initiating with Cluster mode, several threads will run');
     const cluster = require('cluster')
     if (cluster.isPrimary) {
-        console.log('Primary thread, creating workers');
+        console.log(`Primary thread, with process id: ${process.pid}`);
         for (let index = 0; index < 2; index++) {
-            console.log('Creating forks');
+            console.log('Creating a fork');
             cluster.fork()
         }
         cluster.on('exit', (worker, coder, signal) => {
             console.log(`Worker: ${process.pid} has died`);
         }) 
     } else {
-        console.log('Will start listening');
+        console.log(`Creating a new thread, with id ${process.pid}`);
         const Server = require('./server/server')
         const server = new Server()
         server.listen()
