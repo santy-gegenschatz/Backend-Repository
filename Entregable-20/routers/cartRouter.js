d31fde132a5344eb371148c69e7fe68ffe597a04
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2023-01-26 17:21:45 -0300

    [bug fix] - commited missing files of entregable 20

---
+++ Entregable-20/routers/cartRouter.js
@@ -0,0 +1,23 @@
+const { getCart, createCart, addItemToCart, deleteCartItem, deleteCart } = require('../controllers/cartController')
+const { logRouteInfo } = require('../loggers/logger')
+const { Router } = require('express')
+const cartRouter = Router()
+
+// ROUTER URL'S
+// Get a specific cart with all its products
+cartRouter.get('/:id', getCart)
+
+// Create a new empty cart
+cartRouter.post('/', createCart)
+
+// Add a product to a cart
+cartRouter.post('/:id/:id_prod', addItemToCart)
+
+// Delete a product from a cart
+cartRouter.delete('/:id/:id_prod', deleteCartItem)
+
+// Delete a cart
+cartRouter.delete('/:id', deleteCart)
+
+
+exports.cartRouter = cartRouter
