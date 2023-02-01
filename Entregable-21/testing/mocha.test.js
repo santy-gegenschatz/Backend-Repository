e312fe01ad8509a379740e64057b489bcdab65a4
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2023-02-01 20:35:36 -0300

    [feature] - began work to redesign the signup system

--- Entregable-21/testing/mocha.test.js
+++ Entregable-21/testing/mocha.test.js
@@ -1,8 +1,21 @@
 require('dotenv').config() // => Then we can use it anywhere in our Node project.
 const assert = require('assert');
 const fetch = require('node-fetch');
+const { logDebug } = require('../loggers/logger');
 
-describe('Integration test', () => {
+describe('Product API Integration test', () => {
+    before( () => {
+        logDebug('Starting integration test')
+    })
+
+    beforeEach( () => {
+        logDebug('Starting a new test')
+    })
+
+    after( () => {
+        logDebug('Finishing integration test')
+    })
+
     it('should get all the products', async () => {
         console.log(`http://localhost:${process.env.PORT}/api/products`);
         const response = await fetch(`http://localhost:${process.env.PORT}/api/products`)
